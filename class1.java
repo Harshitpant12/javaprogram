a802f1d9d6ca83776e2a0e10bf7365b54fc0c216
Harshitpant12 <harshpant778@gmail.com>
2023-05-17 09:02:16 +0530

    first

---
+++ class1.java
@@ -0,0 +1,47 @@
+class class1 {
+    public static void main(String[] args) {
+        int i,j;
+        for ( i = 1; i<6; i++) {
+            for (j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int k = 1; k <5; k++) {
+            for (int m = 4; m >=k; m--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //now 2nd one
+        // for (int i1 = 6; i1 >1; i1++) {
+        //     for (int j3 = 1; j3>i1; j3++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+        
+        
+    }
+}
