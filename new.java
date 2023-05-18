6b2b3bd3f66d8993a247b7e6e473a50f855621b9
Harshitpant12 <harshpant778@gmail.com>
2023-05-18 09:17:38 +0530

    pull commit

--- new.java
+++ new.java
@@ -1,6 +1,6 @@
 class ghh {
     public static void main(String[] args) {
-        String name = "JahaJ";
+        String name = "harshit";
         String rev = "";
         for (int i = name.length() - 1; i >= 0; i--) {
             rev = rev + name.charAt(i);
